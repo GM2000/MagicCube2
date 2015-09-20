ccb02382bc744ae5140d660406fcdb7e859bd724
GM2000 <gm2000@outlook.com>
2015-09-20 11:02:17 +0800

    动态数组测试

--- MagicCube/src/ShapeGroup.h
+++ MagicCube/src/ShapeGroup.h
@@ -6,31 +6,68 @@ class ShapeGroup
 {
 private:
 	//save size and data
-	struct _Array
+	class _Array
 	{
+	public:
 		GLfloat *SG_Data;
 
 		int		SG_DataSize;
 
-		void inline Reset(int Size)
+		//set the size and clear data
+		void inline SetSize(int Size)
 		{
 			if (Size == 0)
 			{
-				SG_Data		= NULL;
+				SG_Data = NULL;
 				SG_DataSize = 0;
 			}
 			else
 			{
-				SG_Data		= new GLfloat[Size];
+				SG_Data = new GLfloat[Size];
 				SG_DataSize = Size;
 			}
 		}
+		//make the size bigger
+		void inline ExpandSize(unsigned int Size)
+		{
+			GLfloat *SG_Tmp = SG_Data;
+
+			//resize
+			SG_Data = new GLfloat[SG_DataSize + Size];
+			//copy data
+			memcpy(SG_Data, SG_Tmp, SG_DataSize * sizeof(GLfloat));
+			//save size
+			SG_DataSize += Size;
+
+			//delete TMP data
+			delete(SG_Tmp);
+		}
+		//make the size smaller
+		void inline NarrowSize(unsigned int Size)
+		{
+			GLfloat *SG_Tmp = SG_Data;
+
+			//resize
+			SG_Data = new GLfloat[SG_DataSize - Size];
+			//copy data
+			memcpy(SG_Data, SG_Tmp, (SG_DataSize - Size) * sizeof(GLfloat));
+			//save size
+			SG_DataSize -= Size;
+
+			//delete TMP data
+			delete(SG_Tmp);
+		}
 	};
+	struct _Shape
+	{
+		char*	SG_ShapeName;
+	};
+	_Shape*	SG_Shape;
 
 	_Array	SG_VaoData;
 	_Array	SG_TextureData;
 
-	bool SG_Lock;
+	int		SG_Size;
 public:
 	//unload ShaoeGroup
 	~ShapeGroup()
@@ -39,37 +76,31 @@ public:
 		delete(SG_TextureData.SG_Data);
 	}
 	//load ShapeGroup
-	ShapeGroup(int i)
+	ShapeGroup(unsigned int Type)
 	{
-		SG_VaoData.Reset(0);
-		SG_TextureData.Reset(0);
+		SG_VaoData.SetSize(1);
+		SG_TextureData.SetSize(1);
+		SG_TextureData.SG_Data[0] = 213.0;
 
-		SG_Lock = false;
-	}
-	//Lock SG
-	void Lock()
-	{
-		SG_Lock = true;
+		SG_TextureData.ExpandSize(1);
+		SG_TextureData.SG_Data[1] = 13.0;
+		SG_TextureData.NarrowSize(1);
 	}
 	//set SG data
-	int AddShape(GLfloat VBOData[12], GLfloat TexturePosition[8])
+	int AddShape(GLfloat VBOData[12], GLfloat TexturePosition[8],const char* ShapeName)
 	{
-		if (!SG_Lock)
+		SG_Size++;
+
+		for (int i = 0; i < 12; i++)
 		{
-			_Array	TMPData;
-
-			for (int i = 0; i < 12; i++)
-			{
-				SG_TextureData.SG_Data[2 + 0] = VBOData[0];
-				SG_TextureData.SG_Data[2 + 1] = VBOData[1];
-			}
-			for (int i = 0; i < 8; i++)
-			{
-				SG_TextureData.SG_Data[3 + 0] = TexturePosition[0];
-				SG_TextureData.SG_Data[3 + 1] = TexturePosition[1];
-				SG_TextureData.SG_Data[3 + 2] = TexturePosition[2];
-			}
-
+			SG_TextureData.SG_Data[2 + 0] = VBOData[0];
+			SG_TextureData.SG_Data[2 + 1] = VBOData[1];
+		}
+		for (int i = 0; i < 8; i++)
+		{
+			SG_TextureData.SG_Data[3 + 0] = TexturePosition[0];
+			SG_TextureData.SG_Data[3 + 1] = TexturePosition[1];
+			SG_TextureData.SG_Data[3 + 2] = TexturePosition[2];
 		}
 	}
 };
